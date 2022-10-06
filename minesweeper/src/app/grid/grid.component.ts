8295f8e565b1a21f51a2768cf792213b153c463e
smoritz023 <91634909+smoritz023@users.noreply.github.com>
2022-10-06 13:59:42 -0500

    some changes

--- minesweeper/src/app/grid/grid.component.ts
+++ minesweeper/src/app/grid/grid.component.ts
@@ -7,70 +7,60 @@ import { Observable } from 'rxjs';
   styleUrls: ['./grid.component.scss']
 })
 
-
 export class GridComponent implements OnInit {
-
 
   constructor() {
   }
   
   ngOnInit(): void {
-    console.log(this.arr);
-    for(let i = 0; i < this.arrY.length; i++){
-      let temp;
-      for(let j = 0; j < this.arrX.length; j++){
-        if(j == 0){
-          temp = this.arrY[i];
-          this.gridlist.push(temp);
-          temp = this.arrX[j];
-          this.gridlist.push(temp);
-        }
-        else{
-          temp = this.arrX[j];
-          this.gridlist.push(temp);
-        }
 
-        // this.gridlist.push(temp);
-        // console.log(this.gridlist);
+    //putting 2 indidivual arrays into one gidlist array
+    // for(let i = 0; i < this.arrY.length; i++){
+    //   let temp;
+    //   for(let j = 0; j < this.arrX.length; j++){
+        // if(j == 0){
+        //   temp = this.arrY[i];
+        //   this.gridlist.push(temp);
+        //   temp = this.arrX[j];
+        //   this.gridlist.push(temp);
+        // }
+        // else{
+        //   temp = this.arrX[j];
+        //   this.gridlist.push(temp);
+        // }
+    //   }
+    // }
+
+    console.log("init");
+
+    for(let i = 0; i < 2; i++){
+      let temp;
+      for(let j = 0; j < 2; j++){
+
+        temp = this.arra[i][j] = "i" + i + "j" + j;
+        this.gridlist.push(temp);
+        
       }
     }
+
+
     console.log(this.gridlist);
+    console.log("end init");
   }
 
   newFunc(inp: any){
     console.log("newfunc: " + inp);
 
-
-
-    // for(let i = 0; i < this.arrX.length; i++){
-    //   let temp;
-    //   for(let j = 0; j < this.arrY.length; j++){
-    //     if(j == 0){
-    //       temp = this.arrX[i];
-    //       this.gridlist.push(temp);
-    //       temp = this.arrY[j];
-    //       this.gridlist.push(temp);
-    //     }
-    //     else{
-    //       temp = this.arrY[j];
-    //       this.gridlist.push(temp);
-    //     }
-
-    //     // this.gridlist.push(temp);
-    //     // console.log(this.gridlist);
-    //   }
-    // }
     console.log(this.gridlist);
 
+    this.arra[0][0] = "zero zero";
+    this.arra[0][1] = "zero one";
+    this.arra[1][0] = "one zero";
+    this.arra[1][1] = "one one";
+
+    console.log(this.arra);
   }
 
-  arr = [
-    "item 1",
-    "item 2",
-    "item 3",
-    "item 4",
-    "item 5"
-  ];
   columns = 6;
   xHeight = 2;
   yHeight = 1;
@@ -78,15 +68,8 @@ export class GridComponent implements OnInit {
   arrX = ['x1','x2','x3','x4','x5'];
   arrY = ['y6','y7','y8','y9','y10'];
 
-  gridlist:any[] =  [];
+  arra:any[] = [[],[]];
   
-  
-
-
-  inpX = 0;
-  inpY = 0;
-
-
-
+  gridlist:any[] = [];
 
 }
