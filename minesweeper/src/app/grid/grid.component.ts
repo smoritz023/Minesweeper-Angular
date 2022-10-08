4f602e1f9aa967203bde883b6dbad5ab74c6e992
Sam Moritz <91634909+smoritz023@users.noreply.github.com>
2022-10-08 11:28:33 -0500

    ensuring changes

--- minesweeper/src/app/grid/grid.component.ts
+++ minesweeper/src/app/grid/grid.component.ts
@@ -13,31 +13,13 @@ export class GridComponent implements OnInit {
   }
   
   ngOnInit(): void {
-
-    //putting 2 indidivual arrays into one gidlist array
-    // for(let i = 0; i < this.arrY.length; i++){
-    //   let temp;
-    //   for(let j = 0; j < this.arrX.length; j++){
-        // if(j == 0){
-        //   temp = this.arrY[i];
-        //   this.gridlist.push(temp);
-        //   temp = this.arrX[j];
-        //   this.gridlist.push(temp);
-        // }
-        // else{
-        //   temp = this.arrX[j];
-        //   this.gridlist.push(temp);
-        // }
-    //   }
-    // }
-
     console.log("init");
 
     for(let i = 0; i < 2; i++){
       let temp;
       for(let j = 0; j < 2; j++){
 
-        temp = this.arra[i][j] = "i" + i + "j" + j;
+        temp = this.arra[i][j] = "i" + i + " j" + j;
         this.gridlist.push(temp);
         
       }
@@ -51,17 +33,19 @@ export class GridComponent implements OnInit {
   newFunc(inp: any){
     console.log("newfunc: " + inp);
 
-    console.log(this.gridlist);
+    // console.log(this.gridlist);
 
-    this.arra[0][0] = "zero zero";
-    this.arra[0][1] = "zero one";
-    this.arra[1][0] = "one zero";
-    this.arra[1][1] = "one one";
+    // this.arra[0][0] = 0;
+    // this.arra[0][1] = 1;
+    // this.arra[1][0] = 2;
+    // this.arra[1][1] = 3;
 
-    console.log(this.arra);
+    // console.log(this.arra);
+
+    console.log(this.gridlist[inp]);
   }
 
-  columns = 6;
+  columns = 2;
   xHeight = 2;
   yHeight = 1;
 
