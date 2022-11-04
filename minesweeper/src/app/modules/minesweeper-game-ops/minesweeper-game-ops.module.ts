3c4f75b96ca63cb89b419ae00f7b45ba97f00a7a
Sam Moritz <91634909+smoritz023@users.noreply.github.com>
2022-11-04 08:34:08 -0500

    updating changes

--- minesweeper/src/app/modules/minesweeper-game-ops/minesweeper-game-ops.module.ts
+++ minesweeper/src/app/modules/minesweeper-game-ops/minesweeper-game-ops.module.ts
@@ -12,6 +12,27 @@ import { CommonModule } from '@angular/common';
 export class MinesweeperGameOpsModule {
   public newestFunction(){
     console.log("newest function");
+    this.initializeBoard(4,4,15);
+  }
+
+  arra:any[] = [[],[]];
+
+  public initializeBoard(xWidth:number, yWidth:number, numOfMines:number){
+    //we will begin by setting the size of the board
+    // this.arra[yWidth][xWidth];
+    for(let i = 0; i < yWidth; i++){
+      
+      for(let j = 0; j < xWidth; j++){
+        this.arra[[i][j]] = 'i ' + i + ' j ' + j;
+      }
+    }
+    console.log(this.arra);
+    return this.arra;
+
+    //we will then place the mines
+
+
+    //we will then place the numbers
   }
 
  }
